8b3bf2bef4a8095f5ab0befd4fefea2aa2fcef66
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-16 22:32:39 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: {
+    editor: "./src/index.js"
+  },
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "js/[name].bundle.js",
+    publicPath: "/"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|ttf|eot)$/i,
+        type: "asset/resource"
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html"
+    })
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "static")
+    },
+    historyApiFallback: true,
+    hot: true,
+    port: 8601
+  },
+
+  devtool: "source-map"
+};
